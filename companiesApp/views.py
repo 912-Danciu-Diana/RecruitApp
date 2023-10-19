18a615ec4a977f3e8dd66e841127c96e995b010c
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2023-10-19 13:35:24 +0300

    add search filters by name, location, skill

--- companiesApp/views.py
+++ companiesApp/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Company
 from .serializers import CompanySerializer
 
@@ -6,6 +6,8 @@ from .serializers import CompanySerializer
 class CompanyListView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'location__city', 'location__country']
 
 
 class CompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
--- jobsApp/views.py
+++ jobsApp/views.py
@@ -1,11 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Job, Skill, JobSkills
 from .serializers import JobSerializer, SkillSerializer, JobSkillsSerializer
+from django.db.models import Q
 
 
 class JobListView(generics.ListCreateAPIView):
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    def get_queryset(self):
+        queryset = Job.objects.all()
+        search_term = self.request.query_params.get('search', None)
+        if search_term:
+            name_location_filtered = queryset.filter(
+                Q(name__icontains=search_term) |
+                Q(location__city__icontains=search_term) |
+                Q(location__country__icontains=search_term)
+            )
+            skill_jobs = JobSkills.objects.filter(skill__name__icontains=search_term).values_list('job', flat=True)
+            skills_filtered = queryset.filter(id__in=skill_jobs)
+            combined_queryset = (name_location_filtered | skills_filtered).distinct()
+            return combined_queryset
+        return queryset
 
 
 class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
