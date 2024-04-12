932a0c1c7ffb165579f9e13fcb1061883e6a1fe0
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-04-12 12:49:07 +0300

    register design

--- src/screens/RegisterRecruiteeScreen.js
+++ src/screens/RegisterRecruiteeScreen.js
@@ -2,6 +2,20 @@ import React, { useContext, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import loginImg from '../assets/login-img.webp'
+import Textarea from '@mui/joy/Textarea';
+
+const CustomTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: '#000',
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: '#000',
+    }
+});
 
 const RegisterRecruiteeScreen = () => {
     const [username, setUsername] = useState('');
@@ -46,7 +60,6 @@ const RegisterRecruiteeScreen = () => {
             navigate('/userprofile');
         } catch (error) {
             console.error("Registration failed:", error);
-            // You might want to handle this error by displaying an error message to the user
         }
     };
 
@@ -63,153 +76,197 @@ const RegisterRecruiteeScreen = () => {
     };
 
     const styles = {
-        container: {
+        loginContainer: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            width: '100vw',
+            overflow: 'clip',
+            fontFamily: 'Helvetica'
+        },
+        formContainer: {
+            height: '100%',
+            width: '50%',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            height: '100vh',
-            background: 'linear-gradient(to right, #6a11cb 0%, #2575fc 100%)',
+            gap: '1.2em',
+            padding: '0 12%',
+            overflowY: 'auto',
         },
+        imageContainer: {
+            height: '100%',
+            width: '50%',
+            minWidth: '50%',
+            display: 'flex',
+            position: 'relative'
+        },
+        imageOverlay: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '50%',
+            background: 'linear-gradient(90deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0) 100%)',
+            display: 'block'
+        },
+        inputContainer: {
+            width: "100%",
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+            gap: '0.5em'
+        },
+
         input: {
-            margin: '5px',
-            padding: '10px',
-            border: 'none',
-            borderBottom: '2px solid #333',
-            background: 'transparent',
-            outline: 'none',
+            width: '100%'
         },
+
         button: {
-            margin: '10px',
-            padding: '10px 20px',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-            background: '#333',
-            color: 'white',
-            fontSize: '1em',
+            width: '100%',
+            background: '#000',
+            textTransform: 'none',
+            marginTop: '1em'
         },
+
+        icon: {
+            width: '1.2em',
+            height: '1.2em',
+            marginBottom: '0.2em'
+        },
+
+        registerContainer: {
+            marginTop: "2em",
+            fontSize: '0.8em',
+            display: 'flex',
+            gap: '0.8em'
+        },
+
         link: {
-            marginTop: '20px',
-        },
-        linktext: {
-            color: 'black',
-        },
-        fileInput: {
-            cursor: 'pointer',
-            padding: '10px 20px',
-            border: '2px solid #333',
-            borderRadius: '5px',
-            background: 'transparent',
+            textDecoration: 'none',
             fontWeight: 'bold',
-            textAlign: 'center',
-            transition: 'background-color 0.3s',
-            marginTop: '5px',
+            color: '#000'
         },
     };
 
     return (
-        <div style={styles.container}>
-            <input
-                style={styles.input}
-                placeholder="Username *"
-                onChange={e => setUsername(e.target.value)}
-                value={username}
-                required
-            />
-            <input
-                style={styles.input}
-                placeholder="Email *"
-                onChange={e => setEmail(e.target.value)}
-                value={email}
-                type="email"
-                required
-            />
-            <input
-                style={styles.input}
-                placeholder="First Name *"
-                onChange={e => setFirstName(e.target.value)}
-                value={firstName}
-                required
-            />
-            <input
-                style={styles.input}
-                placeholder="Last Name *"
-                onChange={e => setLastName(e.target.value)}
-                value={lastName}
-                required
-            />
-            <input
-                style={styles.input}
-                placeholder="Password *"
-                onChange={e => setPassword(e.target.value)}
-                value={password}
-                type="password"
-                required
-            />
-            <textarea
-                style={styles.input}
-                placeholder="Profile Description"
-                onChange={e => setProfileDescription(e.target.value)}
-                value={profileDescription}
-            />
-            <input
-                style={styles.input}
-                placeholder="School"
-                onChange={e => setSchool(e.target.value)}
-                value={school}
-            />
-            <input
-                style={styles.input}
-                placeholder="University"
-                onChange={e => setUniversity(e.target.value)}
-                value={university}
-            />
-            <textarea
-                style={styles.input}
-                placeholder="Work Experience"
-                onChange={e => setWorkExperience(e.target.value)}
-                value={workExperience}
-            />
-            <>
-                <button className="button-upload" onClick={handleProfilePicClick} style={styles.fileInput}>
-                    Upload a profile pic
-                </button>
-                <input
-                    style={{ display: "none" }}
-                    type="file"
-                    onChange={e => handleFileChange(e, setProfilePic)}
-                    ref={hiddenProfilePicInput}
-                />
-                {profilePic && <p>Uploaded file: {profilePic.name}</p>}
-            </>
-            <>
-                <button className="button-upload" onClick={handleCoverPhotoClick} style={styles.fileInput}>
-                    Upload a cover photo
-                </button>
-                <input
-                    style={{ display: "none" }}
-                    type="file"
-                    onChange={e => handleFileChange(e, setCoverPhoto)}
-                    ref={hiddenCoverPhotoInput}
-                />
-                {coverPhoto && <p>Uploaded file: {coverPhoto.name}</p>}
-            </>
-            <>
-                <button className="button-upload" onClick={handleCvClick} style={styles.fileInput}>
-                    Upload your cv
-                </button>
-                <input
-                    style={{ display: "none" }}
-                    type="file"
-                    onChange={e => handleFileChange(e, setCv)}
-                    ref={hiddenCvInput}
-                />
-                {cv && <p>Uploaded file: {cv.name}</p>}
-            </>
-            <button style={styles.button} onClick={handleRegister}>Register</button>
-            <div style={styles.link}>
-                Already have an account? <Link to="/" style={styles.linktext}>Login here</Link>
+        <div style={styles.loginContainer}>
+            <div style={styles.formContainer}>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    label="Username "
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    required
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="Email *"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    type="email"
+                    required
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="First Name *"
+                    onChange={e => setFirstName(e.target.value)}
+                    value={firstName}
+                    required
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="Last Name *"
+                    onChange={e => setLastName(e.target.value)}
+                    value={lastName}
+                    required
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="Password *"
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    required
+                /></div>
+                <div style={styles.inputContainer}><Textarea
+                    style={styles.input}
+                    placeholder="Profile Description"
+                    onChange={e => setProfileDescription(e.target.value)}
+                    value={profileDescription}
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="School"
+                    onChange={e => setSchool(e.target.value)}
+                    value={school}
+                /></div>
+                <div style={styles.inputContainer}><CustomTextField
+                    style={styles.input}
+                    variant='standard'
+                    placeholder="University"
+                    onChange={e => setUniversity(e.target.value)}
+                    value={university}
+                /></div>
+                <div style={styles.inputContainer}><Textarea
+                    style={styles.input}
+                    placeholder="Work Experience"
+                    onChange={e => setWorkExperience(e.target.value)}
+                    value={workExperience}
+                /></div>
+                <>
+                    <Button style={styles.button} variant="contained" onClick={handleProfilePicClick} >
+                        Upload a profile pic
+                    </Button>
+                    <input
+                        style={{ display: "none" }}
+                        type="file"
+                        onChange={e => handleFileChange(e, setProfilePic)}
+                        ref={hiddenProfilePicInput}
+                    />
+                    {profilePic && <p>Uploaded file: {profilePic.name}</p>}
+                </>
+                <>
+                    <Button style={styles.button} variant="contained" onClick={handleCoverPhotoClick} >
+                        Upload a cover photo
+                    </Button>
+                    <input
+                        style={{ display: "none" }}
+                        type="file"
+                        onChange={e => handleFileChange(e, setCoverPhoto)}
+                        ref={hiddenCoverPhotoInput}
+                    />
+                    {coverPhoto && <p>Uploaded file: {coverPhoto.name}</p>}
+                </>
+                <>
+                    <Button style={styles.button} variant="contained" onClick={handleCvClick} >
+                        Upload your cv
+                    </Button>
+                    <input
+                        style={{ display: "none" }}
+                        type="file"
+                        onChange={e => handleFileChange(e, setCv)}
+                        ref={hiddenCvInput}
+                    />
+                    {cv && <p>Uploaded file: {cv.name}</p>}
+                </>
+                <Button style={styles.button} variant="contained" onClick={handleRegister}>Register</Button>
+                <div style={styles.registerContainer}>
+                    Already have an account? <Link to="/" style={styles.link}>Login here</Link>
+                </div>
+            </div>
+            <div style={styles.imageContainer}>
+                <img src={loginImg} alt="loginImg" />
+                <div style={styles.imageOverlay}></div>
             </div>
         </div>
     );
