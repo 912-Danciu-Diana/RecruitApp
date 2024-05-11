4840a6f8c942fb13a5acace1754bfeeffbfc8a7a
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-05-11 16:50:10 +0300

    applicant screen design

--- src/screens/ApplicantScreen.js
+++ src/screens/ApplicantScreen.js
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
+import '../styles/UserProfile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const ApplicantScreen = () => {
     const location = useLocation();
@@ -74,15 +77,15 @@ const ApplicantScreen = () => {
             return <p><strong>Application status:</strong> Candidate rejected</p>;
         }
 
-        if(currentApplication.accepted) {
+        if (currentApplication.accepted) {
             return <p><strong>Application status:</strong> Accepted for job.</p>;
-          }
+        }
 
         if (currentApplication.acceptedForQuiz === true && quizExists) {
             if (quizTaken) {
                 return <div>
                     <p><strong>Quiz status:</strong> Completed. Score: {quizScore}%</p>
-                    <button onClick={() => navigate('/viewtakenquiz', {state: {quiz_id: quiz.id, score: quizScore}})}>View quiz</button>
+                    <button onClick={() => navigate('/viewtakenquiz', { state: { quiz_id: quiz.id, score: quizScore } })}>View quiz</button>
                     <button onClick={handleAcceptCandidate}>Accept candidate</button>
                     <button onClick={handleRejectCandidate}>Reject candidate</button>
                 </div>;
@@ -103,103 +106,32 @@ const ApplicantScreen = () => {
         );
     };
 
-    const profileStyles = {
-        container: {
-            textAlign: 'center',
-            position: 'relative',
-            background: 'linear-gradient(to bottom, #ffffff 0%,#f0f4f7 100%)',
-            padding: '20px',
-            minHeight: '100vh',
-        },
-        coverPhoto: {
-            width: '100%',
-            height: '150px',
-            borderRadius: '15px',
-            marginBottom: '50px',
-            boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-        },
-        profileInfo: {
-            position: 'absolute',
-            top: '100px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginBottom: '20px',
-            zIndex: 1,
-        },
-        profilePic: {
-            width: '120px',
-            height: '120px',
-            borderRadius: '60px',
-            border: '5px solid white',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-        },
-        profileName: {
-            margin: '10px 0',
-            fontSize: '24px',
-            fontWeight: 'bold',
-            color: '#333',
-        },
-        section: {
-            background: '#fff',
-            padding: '15px',
-            borderRadius: '10px',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.05)',
-            margin: '10px 0',
-            textAlign: 'left',
-        },
-        input: {
-            margin: '5px',
-            padding: '10px',
-            border: '1px solid #ddd',
-            borderRadius: '5px',
-            width: 'calc(100% - 22px)',
-            marginBottom: '20px',
-        },
-        button: {
-            margin: '10px',
-            padding: '10px 20px',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-            background: '#000',
-            color: 'white',
-            fontSize: '1em',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
-        },
-        searchContainer: {
-            paddingTop: '80px',
-        },
-    };
-
     return (
         (applicant && (
-            <div style={profileStyles.container}>
-                <img
-                    src={`http://127.0.0.1:8080${applicant.cover_photo}`}
-                    alt={`${applicant.username}'s cover`}
-                    style={profileStyles.coverPhoto}
-                />
-                <div style={profileStyles.profileInfo}>
-                    <img
-                        src={`http://127.0.0.1:8080${applicant.profile_pic}`}
-                        alt={`${applicant.username}'s profile`}
-                        style={profileStyles.profilePic}
-                    />
-                    <h1 style={profileStyles.profileName}>
-                        {applicant.first_name} {applicant.last_name}
-                    </h1>
-                </div>
+            <div className='body'>
+                <header style={{ background: `url(${`http://127.0.0.1:8080${applicant.cover_photo}`}) no-repeat 50% 20% / cover` }}></header>
 
-                <div style={profileStyles.searchContainer}>
+                <div class="cols__container">
+                    <div class="left__col">
+                        <div class="img_container">
+                            <img
+                                src={`http://127.0.0.1:8080${applicant.profile_pic}`}
+                                alt={`${applicant.username}'s profile`}
+                            />
+                            <span></span>
+                        </div>
+                        <h2>{applicant.first_name} {applicant.last_name}</h2>
+                        {applicant.email && <p>{applicant.email}</p>}
 
-                    <div style={profileStyles.section}>
+                        <div class="content">
+                            {applicant.profile_description && <p><strong>About Me:</strong> {applicant.profile_description}</p>}
+                        </div>
+                    </div>
+
+                    <div class="right__col">
                         {applicant.cv && (
                             <div><strong>CV:</strong> <a href={`http://127.0.0.1:8080${applicant.cv}`} target="_blank" rel="noopener noreferrer">View CV</a></div>
                         )}
-                        {applicant.profile_description && <p><strong>About Me:</strong> {applicant.profile_description}</p>}
                         {applicant.school && <p><strong>School:</strong> {applicant.school}</p>}
                         {applicant.university && <p><strong>University:</strong> {applicant.university}</p>}
                         {applicant.work_experience && <p><strong>Work Experience:</strong> {applicant.work_experience}</p>}
@@ -216,10 +148,9 @@ const ApplicantScreen = () => {
                         <div>
                             {renderApplicationStatus()}
                         </div>
-
+                        <button onClick={() => navigate('/recruiterjobprofile', { state: { job: job } })}>Back</button>
                     </div>
-                    <button style={profileStyles.button} onClick={() => navigate('/recruiterjobprofile', { state: { job: job } })}>Back</button>
-                </div>
+                </div>                
             </div>
         ))
     );
